b73babf62a85299540f66aeefb3e426380bdd7ea
Thomas Wood <thomas.wood@huawei.com>
2018-09-06 12:18:25 -0700

    Added some things

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, request, session, url_for
 from app import app
 from app.forms import SearchForm
-from search import query_for_edge, get_graph
+from search import get_results_for_edge, get_graph
 from visualize_data import visualize_image
 import os
 
@@ -24,13 +24,7 @@ def search():
         object = request.form.get('object', 'chair')
         edge = (subject, relationship, object)
         g = get_graph()
-        q = query_for_edge(g, edge)
-        fnames = []
-        for x in q:
-            fnames.append(visualize_image(x, edges=[edge]))
-        print(edge)
-        print(q)
-        fnames = [fname.split('/')[-1] for fname in fnames]
+        fnames = get_results_for_edge(g, edge)
         session['fnames'] = fnames
         return redirect(url_for('results'))
     else:
--- app/search.py
+++ app/search.py
@@ -5,6 +5,8 @@ from gremlin_python.structure.graph import Graph
 from gremlin_python.process.graph_traversal import __
 from gremlin_python.process.strategies import *
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from visualize_data import visualize_image
+
 
 
 def query_for_edge(g, edge):
@@ -19,3 +21,16 @@ values("image_id").toList()
 def get_graph():
     graph = Graph()
     return graph.traversal().withRemote(DriverRemoteConnection('ws://localhost:8182/gremlin','g'))
+
+def get_results_for_edge(g, edge):
+    q = query_for_edge(g, edge)
+    fnames = []
+    for x in q:
+        try:
+            fnames.append(visualize_image(x, edges=[edge]))
+        except:
+            pass
+    print(edge)
+    print(q)
+    fnames = [fname.split('/')[-1] for fname in fnames]
+    return fnames
